b5955056ea963b1ec7933f793ee068bfb4f6ca95
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-08-31 20:20:13 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+
+StoryBrain StoryBank = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration:BoxDecoration(
+            image:DecorationImage(image: AssetImage('images/background.png')),
+        ),
+        
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    
+                    StoryBank.getStory(),
+                    
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState((){
+                       StoryBank.nextStory(1);
+                    });
+                   
+                  },
+                  style:ElevatedButton.styleFrom(backgroundColor:Colors.red),
+                  child: Text(
+                  
+                    StoryBank.getChoice1(),
+                    
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child:Visibility(
+                  
+                  
+                visible:StoryBank.buttonShouldBeVisible(),
+               
+                child: ElevatedButton(
+                  onPressed: () {
+                   setState((){
+                     StoryBank.nextStory(2);
+                   }) ;
+                    
+                  },
+                  style:ElevatedButton.styleFrom(backgroundColor:Colors.blue),
+                  
+                  child: Text(
+                    StoryBank.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+          ),],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
